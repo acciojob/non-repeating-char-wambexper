36fe85b3b244ec0184ae91c8d5c30de9cccc61b8
PETER WAMBEDDE <wambexpeter@gmail.com>
2023-11-14 16:22:31 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+    const charCount = {};
+
+    // Count the occurrences of each character in the string
+    for (const char of str) {
+        charCount[char] = (charCount[char] || 0) + 1;
+    }
+
+    // Find the first non-repeated character
+    for (const char of str) {
+        if (charCount[char] === 1) {
+            return char;
+        }
+    }
+
+    // Return null if no non-repeated character is found
+    return null;
 }
+
 const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+alert(firstNonRepeatedChar(input));
